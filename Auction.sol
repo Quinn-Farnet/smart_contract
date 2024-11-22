f21684bc9548be6462896ed19b2a0189ad96e7af
Quinn-Farnet <156140801+Quinn-Farnet@users.noreply.github.com>
2024-11-21 21:02:21 -0600

    Update Auction.sol
    added modifiers

--- Auction.sol
+++ Auction.sol
@@ -11,42 +11,52 @@ contract Auction {
     event Log(address indexed sender, string message, uint value);
     event Log(address indexed sender, string message);
 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    modifier auctionActive() {
+        require(auctionInProgress, "The auction is not active");
+        _;
+    }
+
     constructor() {
         owner = msg.sender;
         auctionInProgress = false;
-        minBid = 2;
+        minBid = 0 ether;
     }
 
-    function startAuction( uint minimumBid) public  {
-        require(msg.sender == owner, "Only the owner set minimum bid price");
-        require(!auctionInProgress,"The minimum bid price can only be set before the auction starts");
-        minBid = minimumBid;
+    function startAuction(uint minimumBid) public onlyOwner {
+        require(!auctionInProgress, "The minimum bid price can only be set before the auction starts");
+        
+        currentBidder = address(0); 
+        currentBid = 0;            
+        
+        minBid = minimumBid * 1 ether;
         auctionInProgress = true; 
-        emit Log(msg.sender, "The auction is now open and the starting bid price:",minBid);
+        emit Log(msg.sender, "The auction is now open and the starting bid price:", minBid);
     }
 
-    
-    function bid() public payable {
-        require(msg.value > minBid, "The bid must exceed current bid price");
-        require(auctionInProgress,"Auction is closed at this time");
+    function bid() public payable auctionActive {
+        require(msg.value > minBid, "The bid must exceed current bid price"); 
         refund();
+
         currentBidder = msg.sender;
         currentBid = msg.value;
         minBid = msg.value;
-        emit Log(msg.sender, "The current bid is:", currentBid );
+       
+        emit Log(msg.sender, "The current bid is:", currentBid);
     }
 
-    
-    function closeAuction() public {
-        require(msg.sender == owner, "Only the owner can close the auction");
-        require(auctionInProgress,"Auction is already closed");
+    function closeAuction() public onlyOwner auctionActive {
         payable(owner).transfer(currentBid);
         auctionInProgress = false;
+        
         emit Log(msg.sender, "The auction is now closed.");
     }
 
     function refund() private {
         payable(currentBidder).transfer(currentBid);
-
     }
 }
